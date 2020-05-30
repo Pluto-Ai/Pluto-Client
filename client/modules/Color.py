c9e2bf8cb6bb364cd823660569e3289376d52a6e
ppmclalaj <mattymcmanus@gmail.com>
2020-05-29 20:49:02 -0400

    add favorite color module for jilly

---
+++ client/modules/Color.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8-*-
+import random
+import re
+
+WORDS = ["FAVORITE", "COLOR"]
+
+def handle(text, mic, profile):
+    mic.say("RAW ORANGE")
+
+
+def isValid(text):
+    return bool(re.search(r'\bfavorite color\b', text, re.IGNORECASE))
